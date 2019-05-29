e5761d187617ddd1711a3003051417ed04ccab04
Suyash Kumar <suyashkumar2409@gmail.com>
2019-05-29 15:36:43 +0530

    gitignore is necessary to learn what all is ignored

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # app is the current module. It will soon include a routes file
+# it needs to be declared after making app because routes uses app variable
+# initialised here. Had it been called before, routes would have run
+# and it wouldn't have know which variable app is
 from app import routes
